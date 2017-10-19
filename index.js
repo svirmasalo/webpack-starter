6374840e95988ef83099cb3939d1b237f7f25951
Sampo Virmasalo <info@svirmasalo.fi>
2017-10-19 12:12:44 +0300

    init

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var redom = require('redom');
 const {el, mount} = redom;
 
 console.log("Hello from JavaScript!");
-const someString = 'Oletko siellä watcher?';
+const someString = 'And hello es6!';
 const hello = el('h2', `Hello RE:DOM! ${someString}`);
 mount(document.body, hello);
